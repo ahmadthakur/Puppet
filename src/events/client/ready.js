97c5fd6e3e4384dc15ac9225070d04156c8e77f5
Ahmad Thakur <ahmad.thakur@outlook.com>
2024-05-17 12:40:34 +0500

    chore: Update dependencies and discord-player initialization

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -2,14 +2,20 @@
 // Requirements: colors
 const colors = require("colors");
 
+// Discord Player Initialization
+const { Player } = require("discord-player");
+
 // Export the event
 module.exports = {
   name: "ready",
   execute(client) {
+    const player = new Player(client);
+
     client.on("error", (error) => {
       console.log(colors.bgBlack("[Bot Status]; Bot is offline"));
       console.log(colors.bgRed(error));
     });
+
     console.log(colors.bgGreen("[Bot Status]; Bot is online"));
   },
 };
